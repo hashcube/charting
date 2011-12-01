29aac83d1e8736740e07ce81a6cd4a8c52bed2a2
Deepan Chakravarthy <codeshepherd@gmail.com>
2011-12-01 15:35:46 +0530

    added basic working version

---
+++ sample_conf.php
@@ -0,0 +1,7 @@
+<?php
+  const DBHOST = 'localhost';
+  const USER = 'myuser';
+  const PASS = 'mypass';
+  const DB = 'mydb';
+  const CONFDB = 'mycharts';
+?>
---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+require_once('conf.php');
+require_once('Db.php');
+
+$db = Db::getInstance();
+$data = $db->getCharts();
+//print_r($data);
+//$json_data = json_encode($data, JSON_NUMERIC_CHECK);
+$json_data = json_encode($data, 1);
+//echo "<pre>"; print_r($data); echo "</pre>";
+require_once('templates/index.html');
+?>
