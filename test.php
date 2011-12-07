9b7f68e7077bf7cb7c94e707a78d0ebb106d79cf
Abhinav Singh <abhinav.singh@hashcube.com>
2011-12-07 11:18:41 +0530

    changed json_encode option to JSON_NUMERIC_CHECK

--- test.php
+++ test.php
@@ -5,8 +5,8 @@ require_once('Db.php');
 $db = Db::getInstance();
 $data = $db->getCharts();
 //print_r($data);
-//$json_data = json_encode($data, JSON_NUMERIC_CHECK);
-$json_data = json_encode($data, 1);
+$json_data = json_encode($data, JSON_NUMERIC_CHECK);
+//$json_data = json_encode($data, 1);
 //echo "<pre>"; print_r($data); echo "</pre>";
 require_once('templates/index.html');
 ?>
