e95f61a21b54f4452de886688dc87a6eb3bfb764
Anon Ray <rayanon@hashcube.com>
2011-12-15 17:57:58 +0530

    Code for:   1. Min, Max values   2. Filling missing dates   3. Formatting dates

---
+++ Utils.php
@@ -0,0 +1,45 @@
+<?php
+class Utils
+{
+  /* Format dates according to d D M format*/
+  public static function formatDates($data)
+  {
+    foreach($data as $id=>$charts){
+      foreach($charts['charts'] as $i=>$chart_details){
+        foreach($chart_details['result']['x'] as $j=>$value){
+          $value = date("d D M", strtotime($value));
+          $data[$id]['charts'][$i]['result']['x'][$j] = $value;
+        }
+      }
+    }
+    return $data;
+  }
+
+  public static function fillMissingDates($x ,$y, $startdate, $enddate)
+  {
+    for($k=0; $k < count($x) && self::datediff($enddate, $startdate)>0;  $k++) {
+      if(!in_array($startdate, $x)) {
+        //$x[] = $startdate;
+        $xarr1 = array_slice($x, 0, $k);
+        $yarr1 = array_slice($y, 0, $k);
+        $xarr1[] = $startdate;
+        $yarr1[] = 0;
+        $xarr2 = array_slice($x, $k, count($x)-$k);
+        $yarr2 = array_slice($y, $k, count($y)-$k);
+        $x = array_merge($xarr1, $xarr2);
+        $y = array_merge($yarr1, $yarr2);
+      }
+      $startdate = date("Y-m-d", strtotime("$startdate +1 day"));
+    }
+    return array('x'=>$x, 'y'=>$y);
+  }
+
+  /* returns difference between two dates in days*/
+  public static function datediff($enddate, $startdate)
+  {
+    $secs = strtotime($enddate) - strtotime($startdate);
+    return ((($secs/60)/60)/24);
+  }
+
+}
+?>
--- sudokuquest.php
+++ sudokuquest.php
@@ -1,55 +1,25 @@
 <?php
-require_once('conf.php');
 require_once('Db.php');
+require_once('Utils.php');
 
 $db = Db::getInstance();
 $ids = "1,2,300,400,500,520";
 $data = $db->getSpecificCharts($ids);
-$ids = explode(',', $ids);
-//$data = fix_missing_dates($data, $ids);
+foreach($data as $id=>$charts){
+  if($id != 1 && $id != 2) {
+    foreach($charts['charts'] as $i=>$chart_details){
+      $x = $chart_details['result']['x'];
+      $y = $chart_details['result']['y'];
+      $res = Utils::fillMissingDates($x, $y, '2011-10-21', date("Y-m-d"));
+      $data[$id]['charts'][$i]['result']['x'] = $res['x'];
+      $data[$id]['charts'][$i]['result']['y'] = $res['y'];
+    }
+  }
+}
+$data = Utils::formatDates($data);
 
 $json_data = json_encode($data, JSON_NUMERIC_CHECK);
 //echo "<pre>"; print_r($data); echo "</pre>";
 require_once('templates/index.html');
 
-
-function fix_missing_dates($data, $ids) 
-{
-  $enddate = date("Y-m-d");
-  //$enddate = '2011-10-31';
-
-  $i = current($ids);
-  do {
-    if(preg_match("/Segments/i", $data[$i]['title']) && 
-        preg_match("/Date/i", $data[$i]['x_axis'])) {
-      for($j=0; $j < count($data[$i]['charts']); $j++) {
-        $startdate = "2011-10-21";
-        $x = $data[$i]['charts'][$j]['result']['x'];
-        $y = $data[$i]['charts'][$j]['result']['y'];
-        for($k=0; $k < count($x) && datediff($enddate, $startdate)>0;  $k++) {
-          if(!in_array($startdate, $x)) {
-            //$x[] = $startdate;
-            $xarr1 = array_slice($x, 0, $k);
-            $yarr1 = array_slice($y, 0, $k);
-            $xarr1[] = $startdate;
-            $yarr1[] = 0;
-            $xarr2 = array_slice($x, $k, count($x)-$k);
-            $yarr2 = array_slice($y, $k, count($y)-$k);
-            $x = array_merge($xarr1, $xarr2);
-            $y = array_merge($yarr1, $yarr2);
-          }
-          $startdate = date("Y-m-d", strtotime("$startdate +1 day"));
-        }
-        $data[$i]['charts'][$j]['result']['x'] = $x;
-        $data[$i]['charts'][$j]['result']['y'] = $y;
-      }
-    }
-  }while($i = next($ids));
-  return $data;
-}
-
-function datediff($enddate, $startdate){ // in days
-  $secs = strtotime($enddate) - strtotime($startdate);
-  return ((($secs/60)/60)/24);
-}
 ?>
