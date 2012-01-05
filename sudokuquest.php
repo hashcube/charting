7050ef7e5ad9529ded4ae8393042c5450e75594c
Abhinav Singh <abhinav.singh@hashcube.com>
2012-01-05 18:44:50 +0530

    deleted chartid 5 (returning users graph in suodkuquest dashboard)

--- sudokuquest.php
+++ sudokuquest.php
@@ -6,7 +6,7 @@ require_once('Db.php');
 require_once('Utils.php');
 
 $db = Db::getInstance();
-$ids = "100,200,290,5,300,320,330,350,400,500,520,550,570,580,610,620,630,650,660,700,800,900";
+$ids = "100,200,290,300,320,330,350,400,500,520,550,570,580,610,620,630,650,660,700,800,900";
 $LTV_data = $db->payingUsersLTVQuery();
 $data = $db->getSpecificCharts($ids);
 
